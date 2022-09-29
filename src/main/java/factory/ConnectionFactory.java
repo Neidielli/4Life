9b5bcf9052f1a2d900f2bc081e5e2c6fa63d82e8
Neidielli <100215137+Neidielli@users.noreply.github.com>
2022-09-29 10:32:35 -0300

    commit de cadastro de usuarios

--- src/main/java/factory/ConnectionFactory.java
+++ src/main/java/factory/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/4Life", "root", "UTF2022t7Usa");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/projetojava", "root", "Jackson23+-");
         } 
         catch (SQLException exc) {
             throw new RuntimeException(exc);
