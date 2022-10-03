585ec45e10bcb1211776518a02a8d06d141410a4
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2022-10-02 23:29:33 -0300

    cadastro de funcionario

--- src/main/java/DAO/funcionarioDAO.java
+++ src/main/java/DAO/funcionarioDAO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO;
+package dao;
 /**
  * @author tayna
  */
@@ -12,56 +12,60 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
 import model.Funcionario;
 
 
 public class funcionarioDAO {
-    private Connection connection = null;
+    private Connection connection;
+    int id;
+    String nome;
+    String CPF;
+    String tipo_func;
+    String telefone;
+    String email;
+    String especialidade;
     
     public funcionarioDAO(){ 
         this.connection = new ConnectionFactory().getConnection();
     } 
 
     public boolean cadastrar(Funcionario funcionario){ 
-        String sql = "INSERT INTO funcionario(id,nome,CPF,tipo_func,telefone,email,senha,especialidade) VALUES(?,?,?,?,?,?,?,?)";
-        PreparedStatement pdstmt = null;
+        String sql = "INSERT INTO funcionario(nome,CPF,tipo_func,telefone,email,especialidade) VALUES(?,?,?,?,?,?)";
+  
         try { 
-            pdstmt = connection.prepareStatement(sql);
-            pdstmt.setInt(1, funcionario.getId());
-            pdstmt.setString(2, funcionario.getNome());
-            pdstmt.setString(3, funcionario.getCPF());
-            pdstmt.setString(4, funcionario.getTipo_func());
-            pdstmt.setString(5, funcionario.getTelefone());
-            pdstmt.setString(6, funcionario.getEmail());
-            pdstmt.setString(7, funcionario.getSenha());
-            pdstmt.setString(8, funcionario.getEspecialidade());
+            PreparedStatement pdstmt = connection.prepareStatement(sql);
+            
+            pdstmt.setString(1, funcionario.getNome());
+            pdstmt.setString(2, funcionario.getCPF());
+            pdstmt.setString(3, funcionario.getTipo_func());
+            pdstmt.setString(4, funcionario.getTelefone());
+            pdstmt.setString(5, funcionario.getEmail());
+            pdstmt.setString(6, funcionario.getEspecialidade());
             pdstmt.executeUpdate();
+            pdstmt.close();
             
             return true;
         } 
         catch (SQLException exc) { 
             System.err.println("Erro ao cadastrar funcionário!"+exc);
+            //throw new RuntimeException(exc);
             
             return false;
-            //throw new RuntimeException(exc);
         } 
-        finally{
-            ConnectionFactory.closeConnection(connection, pdstmt);
-        }
     } 
     
     public List<Funcionario> listaFunc(){
-        this.connection = new ConnectionFactory().getConnection();
+        /*this.connection = new ConnectionFactory().getConnection();
         PreparedStatement pdstmt = null;
-        ResultSet rs = null;
-        
-        List<Funcionario> listaFunc = new ArrayList<>();
-        
+        ResultSet rs = null;*/
         try {
-            pdstmt = connection.prepareStatement("SELECT * from funcionario");
-            rs = pdstmt.executeQuery();
+            List<Funcionario> lista = new ArrayList<Funcionario>();
+            String sql = "SELECT * from funcionario";
+            PreparedStatement pdstmt = connection.prepareStatement(sql);
+            ResultSet rs = pdstmt.executeQuery();
             
             while(rs.next()){
                 Funcionario funcionario = new Funcionario();
@@ -72,15 +76,58 @@ public class funcionarioDAO {
                 funcionario.setTelefone(rs.getString("telefone"));
                 funcionario.setTipo_func(rs.getString("tipo_func"));
                 funcionario.setEspecialidade(rs.getString("especialidade"));
-                listaFunc.add(funcionario);
+                lista.add(funcionario);
             }
+            return lista;
         }
         catch (SQLException exc) {
-            Logger.getLogger(funcionarioDAO.class.getName()).log(Level.SEVERE, null, exc);
+            throw new RuntimeException(exc);
         }
-        finally{
-            ConnectionFactory.closeConnection(connection, pdstmt, rs);
-        }
-        return listaFunc;
+    }
+    
+    public boolean editar(Funcionario func){ 
+        String upsql = "UPDATE funcionario set nome=?, CPF=?, tipo_func=?, telefone=?, email=?, especialidade=? WHERE id=?";
+  
+        try { 
+            PreparedStatement pdstmt = connection.prepareStatement(upsql);
+            
+            pdstmt.setString(1, func.getNome());
+            pdstmt.setString(2, func.getCPF());
+            pdstmt.setString(3, func.getTipo_func());
+            pdstmt.setString(4, func.getTelefone());
+            pdstmt.setString(5, func.getEmail());
+            pdstmt.setString(6, func.getEspecialidade());
+            pdstmt.setInt(7, func.getId());
+            pdstmt.executeUpdate();
+            pdstmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+            return true;
+        } 
+        catch (SQLException exc) { 
+            System.err.println("Erro ao atualizar funcionário!"+exc);
+            //throw new RuntimeException(exc); 
+            return false;
+        } 
+    }
+    
+    public boolean deletar(Funcionario func){ 
+        String delsql = "DELETE from funcionario WHERE id=?";
+  
+        try { 
+            PreparedStatement pdstmt = connection.prepareStatement(delsql);
+           
+            pdstmt.setInt(1, func.getId());
+            pdstmt.executeUpdate();
+            pdstmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+            return true;
+        } 
+        catch (SQLException exc) { 
+            System.err.println("Erro ao deletar funcionário!"+exc);
+            //throw new RuntimeException(exc); 
+            return false;
+        } 
     }
 }
---
+++ src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import factory.ConnectionFactory;
+import model.Usuario;
+import java.sql.*;
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author neidi
+ */
+public class UsuarioDAO {
+    private Connection connection;
+    Long id;
+    String nome;
+    String cpf;
+    String email;
+    String telefone;
+    String senha;
+    public UsuarioDAO(){
+        this.connection = new ConnectionFactory().getConnection();
+    }
+    public void adiciona(Usuario usuario) {
+        String sql = "INSERT INTO usuario(nome,cpf,email,telefone,senha)VALUES(?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getCpf());
+            stmt.setString(3, usuario.getEmail());
+            stmt.setString(4, usuario.getTelefone());
+            stmt.setString(5, usuario.getSenha());
+            
+            stmt.execute();
+            stmt.close();
+        }
+        catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+    } // fim do método adiciona usuário.
+    
+    // Método listar todos os clientes
+    public List<Usuario> listarUsuarios(){
+        try{
+            // vetor que armazena os registro do bd
+            List<Usuario> lista = new ArrayList<Usuario>();
+            // comando sql que lista os dados
+            String sql = "select * from usuario";
+            
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            
+            // o resultado do select será guardado dentro do obj resultSet
+            ResultSet rs = stmt.executeQuery();
+            // laço de repetição para guardar os registros na lista
+            while(rs.next()){
+                Usuario u = new Usuario();
+                u.setId(rs.getLong("id"));
+                u.setNome(rs.getString("nome"));
+                u.setCpf(rs.getString("cpf"));
+                u.setEmail(rs.getString("email"));
+                u.setTelefone(rs.getString("telefone"));
+                    
+                lista.add(u);
+            }
+            return lista;
+        }catch(SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+    // inicio do método Login.
+    public boolean login(String email, String senha) {
+        try {
+            // comando sql
+            String sql = "select * from usuario where email=? and senha=?";
+            
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            // indica ao java quem é quem.
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            
+            // executa
+            ResultSet rs = stmt.executeQuery();
+            //verifica se existe registro
+            if(rs.next()) { // realiza o login
+                return true;
+            }
+                
+        } catch (Exception e) {
+        
+        }
+        return false;
+    }
+        
+}
