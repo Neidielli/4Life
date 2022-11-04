79d3bfe2a36d29267ce58e3950e291ee925d63a4
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2022-11-04 02:24:01 -0300

    Cadastro de pacientes

--- sql bd/paciente.sql
+++ sql bd/paciente.sql
@@ -3,19 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
  */
 /**
- * Author:  neidi
- * Created: 28/10/2022
+ * Author:  tayna
+ * Created: 31 de out. de 2022
  */
 use projetojava;
 create table paciente (
     id int NOT NULL AUTO_INCREMENT,
     nome varchar(200),
-    cpf varchar(15) NOT NULL,
-    email varchar(200) NOT NULL,
+    cpf varchar(15) NOT NULL UNIQUE,
+    email varchar(80) NOT NULL UNIQUE,
     telefone varchar(15) NOT NULL,
     data_nascimento date NOT NULL,
-    endereco varchar(255) NOT NULL,
-    convenio boolean,
-    PRIMARY KEY(id),
-    PRIMARY KEY(nome)
-    );
+    cep varchar(11) NOT NULL,
+    rua varchar(80) NOT NULL,
+    bairro varchar(80) NOT NULL,
+    cidade varchar(80) NOT NULL,
+    estado varchar(50) NOT NULL,
+    num_endereco int NOT NULL,
+    PRIMARY KEY(id, nome)
+);
