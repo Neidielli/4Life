e4556bdf9f4255de65788ea228c15856485a8317
Neidielli <100215137+Neidielli@users.noreply.github.com>
2022-11-10 09:11:09 -0300

    Teste da issue #1

--- src/test/java/DAO/UsuarioDAOTest.java
+++ src/test/java/DAO/UsuarioDAOTest.java
@@ -107,7 +107,7 @@ public class UsuarioDAOTest {
 //        assertEquals(expResult, result);
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-        
+ // =======================================================================================       
 //        UsuarioDAO usuario = new UsuarioDAO();
 //        
 //        String email = "neid@teste.com";
