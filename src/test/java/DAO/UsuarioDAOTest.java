a64bc45948446a37c77b9046483ac2125e86f8eb
Neidielli <100215137+Neidielli@users.noreply.github.com>
2022-11-12 11:19:43 -0300

    teste login usuario

--- src/test/java/DAO/UsuarioDAOTest.java
+++ src/test/java/DAO/UsuarioDAOTest.java
@@ -42,90 +42,102 @@ public class UsuarioDAOTest {
     /**
      * Test of adiciona method, of class UsuarioDAO.
      */
-    @Test
-    public void testAdiciona() {
-        System.out.println("adiciona");
-        Usuario usuario = null;
-        UsuarioDAO instance = new UsuarioDAO();
-        instance.adiciona(usuario);
-        // TODO review the generated test code and remove the default call to fail.
+//    @Test
+//    public void testAdiciona() {
+//        System.out.println("adiciona");
+//        Usuario usuario = null;
+//        UsuarioDAO instance = new UsuarioDAO();
+//        instance.adiciona(usuario);
+//         TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of listarUsuarios method, of class UsuarioDAO.
-     */
-    @Test
-    public void testListarUsuarios() {
-        System.out.println("listarUsuarios");
-        UsuarioDAO instance = new UsuarioDAO();
-        List<Usuario> expResult = null;
-        List<Usuario> result = instance.listarUsuarios();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+//    }
+//
+//    /**
+//     * Test of listarUsuarios method, of class UsuarioDAO.
+//     */
+//    @Test
+//    public void testListarUsuarios() {
+//        System.out.println("listarUsuarios");
+//        UsuarioDAO instance = new UsuarioDAO();
+//        List<Usuario> expResult = null;
+//        List<Usuario> result = instance.listarUsuarios();
+//        assertEquals(expResult, result);
+//         TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of alterar method, of class UsuarioDAO.
-     */
-    @Test
-    public void testAlterar() {
-        System.out.println("alterar");
-        Usuario usuario = null;
-        UsuarioDAO instance = new UsuarioDAO();
-        instance.alterar(usuario);
-        // TODO review the generated test code and remove the default call to fail.
+//    }
+//
+//    /**
+//     * Test of alterar method, of class UsuarioDAO.
+//     */
+//    @Test
+//    public void testAlterar() {
+//        System.out.println("alterar");
+//        Usuario usuario = null;
+//        UsuarioDAO instance = new UsuarioDAO();
+//        instance.alterar(usuario);
+//         TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of excluir method, of class UsuarioDAO.
-     */
-    @Test
-    public void testExcluir() {
-        System.out.println("excluir");
-        Usuario usuario = null;
-        UsuarioDAO instance = new UsuarioDAO();
-        instance.excluir(usuario);
-        // TODO review the generated test code and remove the default call to fail.
+//    }
+//
+//    /**
+//     * Test of excluir method, of class UsuarioDAO.
+//     */
+//    @Test
+//    public void testExcluir() {
+//        System.out.println("excluir");
+//        Usuario usuario = null;
+//        UsuarioDAO instance = new UsuarioDAO();
+//        instance.excluir(usuario);
+//         TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-    }
+//    }
 
     /**
      * Test of login method, of class UsuarioDAO.
      */
     @Test
-    public void testLogin() {
+    public void testLoginUsuario1Verdadeiro() {
         
-//        System.out.println("login");
-//        String email = "neidielli@teste.com";
-//        String senha = "senhateste";
-//        UsuarioDAO instance = new UsuarioDAO();
-//        boolean expResult = false;
-//        boolean result = instance.login(email, senha);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
- // =======================================================================================       
-//        UsuarioDAO usuario = new UsuarioDAO();
-//        
-//        String email = "neid@teste.com";
-//        String senha = "senhatest";
-//        String nome = "neidi";
-//        String telefone = "(00)00001-7896";
-//        String cpf = "00000000000";
-//        int id = "98";
-//       
-//        
-//        /*cadastra usuário*/
-//        assertEquals(true, usuario.adiciona(id, nome, telefone, cpf, email, senha));
-//        
-//        /*executa login*/
-//        assertEquals(true, usuario.login(email, senha));
-//        
-//        /*executa login com dados incorretos*/
-//        assertEquals(false, usuario.login("email", "password"));
-    }
-    
+        // Com Login existente
+        System.out.println("login existente");
+        String email = "neidielli@teste.com";
+        String senha = "senhateste";
+        UsuarioDAO instance = new UsuarioDAO();
+        boolean expResult = true;
+        boolean result = instance.login(email, senha);
+        assertEquals(expResult, result);
+    }    
+         //Com Login verdadeiro
+    @Test    
+    public void testLoginUsuario2Verdadeiro() {
+        System.out.println("login existente usuario 2");
+        String email = "taynara@teste.com";
+        String senha = "senhataynara";
+        UsuarioDAO instance = new UsuarioDAO();
+        boolean expResult = true;
+        boolean result = instance.login(email, senha);
+        assertEquals(expResult, result);
+    }  
+    @Test
+    public void testLoginUsuario1Falso() {
+        
+        // Com Login falso
+        System.out.println("login Falso usuario 2");
+        String email = "neidielli@teste1.com";
+        String senha = "senhateste1";
+        UsuarioDAO instance = new UsuarioDAO();
+        boolean expResult = false;
+        boolean result = instance.login(email, senha);
+        assertEquals(expResult, result);
+    }  
+    @Test    
+    public void testLoginUsuario2Falso() {
+        System.out.println("login falso usuario 2");
+        String email = "taynara@teste1.com";
+        String senha = "senhataynara1";
+        UsuarioDAO instance = new UsuarioDAO();
+        boolean expResult = false;
+        boolean result = instance.login(email, senha);
+        assertEquals(expResult, result);
+    }  
 }
