c5721e052f9beea93523cb2f4bc4f0db73d980b1
Neidielli <100215137+Neidielli@users.noreply.github.com>
2022-11-30 16:35:19 -0300

    Construindo metodo cadastrar Prontuario

--- src/main/java/DAO/ProntuarioDAO.java
+++ src/main/java/DAO/ProntuarioDAO.java
@@ -4,42 +4,43 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import model.Prontuario;
+
 /**
  *
  * @author tayna
  */
 public class ProntuarioDAO {
-//    int idProntuario;
-//    int idPaciente;
-//    Date dataNacimento;
-//    String genero;
-//    String profissão;
-//    String cpfPaciente;
-//    String cidadePaciente;
-//    String cep;
-//    String estado;
-//    String telefone;
-//    String email;
-//    String convenio;
-//    String plano;
-//    String numero;
-//    String medicação;
-//    String alergia;
-//    String observação;
-//    
-//    
-//    
-//    public boolean AgendarConsulta(Agenda agenda){ 
+    private Connection connection;
+    
+    int idProntuario;
+    int id_paciente;
+    String cirurgia;
+    String gestante;
+    String fumante;
+    String infarto;
+    String medicacao;
+    String alergia;
+    String tratamento;
+    String falta_ar;
+    String doenca;
+    String diabetico;
+    String observação;
+    
+    
+    
+//    public boolean cadastrar(Prontuario prontuario){ 
 // 
 //        try { 
 //            
-//            java.util.Date utilDate1 = TelaNovoAgendamento.campoData.getDate(); // DATE
-//            java.sql.Date sqlDate1 = new java.sql.Date(utilDate1.getTime()); // DATE
 //            
 //            // seleciona os campos da tabela
-//            String sqlSelect = "select * from consultas where idProfissional='" + agenda.getFuncionario().getId() +"'" +
-//                    "and data_hora = '" + sqlDate1 + "'" +
-//                    "and hora = '" + TelaNovoAgendamento.campoHora.getSelectedItem() + "'"; 
+//            String sqlSelect = "select * from prontuario where id_paciente='" + prontuario.getPaciente().getId() +"'";
+//            
 //            PreparedStatement stmtSelect = connection.prepareStatement(sqlSelect);
 //            
 //            // o resultado do select será guardado dentro do obj resultSet
@@ -47,24 +48,25 @@ public class ProntuarioDAO {
 //
 //            // condição para verificar se o obj resultSet já existe
 //            if(rs.next()) {
-//                JOptionPane.showMessageDialog(null, "Horário Indisponível",
+//                JOptionPane.showMessageDialog(null, "Prontuario de Paciente já cadastrado",
 //                    "ERRO!", JOptionPane.ERROR_MESSAGE);
 //                return false;
 //            } else {
-//                String sql = "INSERT INTO consultas(procedimento,idPaciente,idProfissional,data_hora,tipo_consulta,hora) VALUES(?,?,?,?,?,?)";
+//                String sql = "INSERT INTO prontuario(id_paciente,cirurgia,gestante,fumante,infarto,medicacao,alergia,tratamento,falta_ar,doenca,diabetico,observacao) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 //                PreparedStatement pdstmt = connection.prepareStatement(sql);
 //
-//                java.util.Date utilDate = agenda.getData_hora(); // DATE
-//                java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime()); // DATE
-//
-//                pdstmt.setString(1, agenda.getProcedimento());
-//
-//                pdstmt.setInt(2, agenda.getPaciente().getId()); //precisa captar o objeto
-//                pdstmt.setInt(3, agenda.getFuncionario().getId()); // idem
-//
-//                pdstmt.setDate(4, sqlDate); // DATE
-//                pdstmt.setString(5, agenda.getTipo_consulta());
-//                pdstmt.setString(6, agenda.getHora());
+////                pdstmt.setInt(1, prontuario.getPaciente().getId()); //precisa captar o objeto
+////                pdstmt.setString(2, prontuario.get); 
+////                pdstmt.setString(3, prontuario.getTipo_consulta());
+////                pdstmt.setString(4, prontuario.getHora());
+////                pdstmt.setString(5, prontuario.getHora());
+////                pdstmt.setString(6, prontuario.getHora());
+////                pdstmt.setString(7, prontuario.getHora());
+////                pdstmt.setString(8, prontuario.getHora());
+////                pdstmt.setString(9, prontuario.getHora());
+////                pdstmt.setString(10, prontuario.getHora());
+////                pdstmt.setString(11, prontuario.getHora());
+////                pdstmt.setString(12, prontuario.getHora());
 //
 //                JOptionPane.showMessageDialog(null, "Consulta cadastrado com sucesso!! ");
 //                pdstmt.execute();
@@ -72,11 +74,11 @@ public class ProntuarioDAO {
 //
 //                return true;
 //            }
-//            
-//        } catch (SQLException exc) { 
-//            System.out.println("Erro ao agendar consulta: " + exc); 
-//            throw new RuntimeException(exc);
-//        }
+////            
+////        } catch (SQLException exc) { 
+////            System.out.println("Erro ao agendar consulta: " + exc); 
+////            throw new RuntimeException(exc);
+////        }
 //    }
     
 }
